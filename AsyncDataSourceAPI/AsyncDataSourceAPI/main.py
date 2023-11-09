d76738ba9796298eda011684d10e1a54141c89e2
Meruzhan Unanyan <unanyanmeruzan@gmail.com>
2023-11-09 19:16:16 +0400

    Enhanced /data endpoint: handle errors, timeouts, and sort results by ID

--- AsyncDataSourceAPI/AsyncDataSourceAPI/main.py
+++ AsyncDataSourceAPI/AsyncDataSourceAPI/main.py
@@ -8,9 +8,18 @@ app = FastAPI()
 
 DATABASE_URL = "data.db"
 
+
 async def get_data_from_db(db, query):
+    # TODO move TimeoutError simulation to test
+    # await asyncio.sleep(2)
     async with db.execute(query) as cursor:
         return await cursor.fetchall()
+
+
+async def get_data_from_db_with_timeout(db, query, timeout=2):
+    result = await asyncio.wait_for(get_data_from_db(db, query), timeout=timeout)
+    return result
+
 
 @app.get("/data")
 async def get_data():
@@ -20,24 +29,28 @@ async def get_data():
         query_3 = "SELECT * FROM data_3"
 
         tasks = [
-            get_data_from_db(db, query_1),
-            get_data_from_db(db, query_2),
-            get_data_from_db(db, query_3)
+            get_data_from_db_with_timeout(db, query_1),
+            get_data_from_db_with_timeout(db, query_2),
+            get_data_from_db_with_timeout(db, query_3),
         ]
 
-        data_1, data_2, data_3 = await asyncio.gather(*tasks)
+        try:
+            data_1, data_2, data_3 = await asyncio.gather(*tasks)
 
-        # Convert the result into dictionaries
-        formatted_data_1 = [{"id": row[0], "name": row[1]} for row in data_1]
-        formatted_data_2 = [{"id": row[0], "name": row[1]} for row in data_2]
-        formatted_data_3 = [{"id": row[0], "name": row[1]} for row in data_3]
+            # Convert the result into dictionaries
+            formatted_data_1 = [{"id": row[0], "name": row[1]} for row in data_1]
+            formatted_data_2 = [{"id": row[0], "name": row[1]} for row in data_2]
+            formatted_data_3 = [{"id": row[0], "name": row[1]} for row in data_3]
 
-        all_data = formatted_data_1 + formatted_data_2 + formatted_data_3
+            all_data = formatted_data_1 + formatted_data_2 + formatted_data_3
+            all_data = sorted(all_data, key=lambda all_data: all_data["id"])
 
-        json_data = json.dumps(all_data, ensure_ascii=False)
+            return all_data
+        except Exception as e:
+            # TODO use logging module
+            print(e)
+            return []
 
-        # Return the JSON-formatted string
-        return json_data
 
 if __name__ == "__main__":
     # Create tables, insert data into tables
@@ -46,4 +59,5 @@ if __name__ == "__main__":
 
     # run server
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
